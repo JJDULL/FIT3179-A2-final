8693508480475c3d8dfc696bc877041b9cb99822
JJDULL <130894454+JJDULL@users.noreply.github.com>
2023-10-15 16:26:19 +1100

    add files

---
+++ js/FIT3179_A2.js
@@ -0,0 +1,15 @@
+var vg_1 = "js/A2_1.vg.json";
+vegaEmbed("#g1", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "js/A2_2.vg.json";
+vegaEmbed("#g2", vg_2).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "js/A2_3.vg.json";
+vegaEmbed("#g3", vg_3).then(function(result) {
+}).catch(console.error);
+
+var vg_4 = "js/A2_4.vg.json";
+vegaEmbed("#g4", vg_4).then(function(result) {
+}).catch(console.error);
